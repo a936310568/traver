48616e73f16d983930cbb1271ce57d6292030cea
a9363 <936310568@qq.com>
2020-12-16 16:22:14 +0800

    第三次提交

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      输入城市或游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="arrow-right">></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style>
+    /* @import url('../../../assets/styles/varibles.styl'); */
+    .header {
+      display: flex;
+      background: #00bcd4;
+      height: .86rem;
+      line-height: .86rem;
+      color: #fff;
+    }
+    .header-left {
+      width: .64rem;
+      float: left;
+    }
+    .back-icon {
+      margin-left: .1rem;
+    }
+    .header-input {
+      flex: 1;
+      height: .64rem;
+      line-height: .64rem;
+      margin-top: .12rem;
+      margin-left: .12rem;
+      background: #fff;
+      border-radius: .1rem;
+      font-size: .14rem;
+      color: #ccc;
+      padding-left: .2rem;
+    }
+    .header-right {
+      width: 1.24rem;
+      float: right;
+      text-align: center;
+    }
+    .arrow-right {
+      font-size: .2rem;
+      color: #ccc;
+    }
+</style>
