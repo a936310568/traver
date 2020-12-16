7a43bff38625146b5bfb320b064cef0e91086685
a9363 <936310568@qq.com>
2020-12-16 18:05:23 +0800

    轮播

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
+// 导入组件
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    // 将组件引入
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div wrapper>
+  <swiper :options="swiperOptions">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <!-- 冒号进行绑定 -->
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://tse1-mm.cn.bing.net/th/id/OIP.xiRbchgw9Vpv78ENDeekZwHaDt?w=340&h=175&c=7&o=5&dpr=1.25&pid=1.7'
+      }, {
+        id: '0002',
+        imgUrl: 'https://tse2-mm.cn.bing.net/th/id/OIP.L0ihzn3t6e58DuX5j8XN_QHaCs?w=304&h=127&c=7&o=5&dpr=1.25&pid=1.7'
+      }, {
+        id: '0003',
+        imgUrl: 'https://tse1-mm.cn.bing.net/th/id/OIP.nsB0xckB4XTgmCxnoJi4LQHaCg?w=344&h=118&c=7&o=5&dpr=1.25&pid=1.7'
+      }, {
+        id: '0004',
+        imgUrl: 'https://tse1-mm.cn.bing.net/th/id/OIP.8WKvQcMuQ4gmLasD3MH3dQHaCg?w=333&h=118&c=7&o=5&dpr=1.25&pid=1.7'
+      }, {
+        id: '0005',
+        imgUrl: 'https://tse1-mm.cn.bing.net/th/id/OIP.dmFNvM6IZ2yf5Wo-7KnU8QHaDI?w=332&h=147&c=7&o=5&dpr=1.25&pid=1.7'
+      }]
+    }
+  }
+}
+</script>
+<style>
+    .wrapper {
+        overflow: hidden;
+        width: 100%;
+        height: 0;
+        padding-bottom: 25%;
+    }
+    .swiper-img {
+        width: 100%;
+        height: 3rem;
+    }
+</style>
