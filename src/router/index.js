4f68af0951590c6f11d10ef36edcaff21481fda8
a936310568 <936310568@qq.com>
2020-12-19 16:53:57 +0800

    detail

--- src/router/index.js
+++ src/router/index.js
@@ -3,22 +3,21 @@ import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
 import Detail from '@/pages/detail/Detail'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }, {
-      path: '/city',
-      name: '.city',
-      component: City
-    }, {
-      path: '/detail/:id',
-      name: 'Detail',
-      component: Detail
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  }, {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: Detail
+  }]
 })
